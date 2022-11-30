bff8148c1134e6a19068154a8f0c3471a87ade4f
Purpete <alberto.prieto.romero@gmail.com>
2022-11-30 15:09:24 +0100

    ultima

--- main.tf
+++ main.tf
@@ -12,6 +12,14 @@ terraform {
   }
 }
 
+provider "azurerm" {
+  subscription_id = "3fc95c44-856c-4cf4-bba4-e47a118a15e0" 
+  client_id = "736b4a43-569b-4821-83fa-ae64021824a7" 
+  client_secret = "nXs8Q~HHvKwGKsRG2pJUZDzXcK27syqHW_wFeaQm" 
+  tenant_id = "d1c80090-db78-4d4d-8950-d47ed6cc756b" 
+  features {}
+}
+
 module "network_vpc_west" {
   source              = "./Modules/Network"
   location            = var.location
